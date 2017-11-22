c1215a3b743920f11074d2999b12173d6f3f9f17
Katie D'Angelo <kd.dangelo@gmail.com>
2017-11-21 19:16:59 -0500

    Adding tests from class with firebase

---
+++ dev/scripts/donut.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Donut extends React.Component {
+    render() {
+        renter (
+            <h2>{this.props.donutName}</h2>
+        )
+    }
+}
+// only one export per module per default
+export default Donut;
---
+++ dev/scripts/firebase.js
@@ -0,0 +1,10 @@
+/ Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyBV73LfoJV9Nj5VEkyZrP1cGRciN5t0agg",
+    authDomain: "potluck-project4.firebaseapp.com",
+    databaseURL: "https://potluck-project4.firebaseio.com",
+    projectId: "potluck-project4",
+    storageBucket: "potluck-project4.appspot.com",
+    messagingSenderId: "749232518595"
+    };
+    firebase.initializeApp(config);
